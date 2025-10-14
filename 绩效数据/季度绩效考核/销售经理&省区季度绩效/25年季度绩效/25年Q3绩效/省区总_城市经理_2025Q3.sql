7d8a75e7ab9b30188ec1718fa7dacb6c1143316f
pengch <jopch@163.com>
2025-10-14 19:26:36 +0800

    1

--- 绩效数据/季度绩效考核/销售经理&省区季度绩效/25年季度绩效/25年Q3绩效/省区总_城市经理_2025Q3.sql
+++ 绩效数据/季度绩效考核/销售经理&省区季度绩效/25年季度绩效/25年Q3绩效/省区总_城市经理_2025Q3.sql
@@ -761,8 +761,121 @@ group by  region_name,
 
 
 
+-- AB类客户按照等级销售经理 20251014 调整
+-- AB类客户明细
+with   tmp_customer_level as (select
+  region_name,
+  province_name,
+  city_group_name,
+  customer_no,
+  customer_name,
+  first_category_name,
+  second_category_name,
+  last_order_date,			-- 最后一次下单时间 需要注意这个是全业务的，下季度要调整20250731
+  work_no,
+  sales_name,
+  sales_value,
+  profit,
+  profit_rate,
+  customer_large_level,
+  customer_small_level,
+  customer_level_tag,
+  if_new_order_cus,
+  first_order_date,
+  quarter
+from
+    csx_analyse.csx_analyse_report_sale_customer_level_qf
+where
+  tag = '2'
+  and quarter = '20252'
+  and customer_large_level in ('A','B')
+  ),
+tmp_sale_detail as   ( 
+    SELECT
+        customer_code,
+        MAX(sdt) AS max_sdt,
+        DATEDIFF('2025-09-30', FROM_UNIXTIME(UNIX_TIMESTAMP(MAX(sdt), 'yyyyMMdd'), 'yyyy-MM-dd')) AS diff_days
+    FROM csx_dws.csx_dws_sale_detail_di
+    WHERE sdt >= '20250701' AND sdt <= '20250930'
+      AND business_type_code = '1'
+      AND order_channel_code <> 4
+    GROUP BY customer_code
+),
+tmp_terminate_info as 
+(select customer_code,terminate_date,
+row_number()over(partition by customer_code order by terminate_date desc ) as rn from 
+(select customer_code,terminate_date from csx_dim.csx_dim_crm_terminate_customer  
+    where sdt='current' 
+        and business_attribute_code=1
+        and status=2
+ union all 
+ select customer_code,terminate_date from csx_dim.csx_dim_crm_terminate_customer_attribute  
+    where sdt='current'  
+        and business_attribute_code=1
+        and approval_status=2
+) a 
+)
+select region_name,
+    province_name,
+    city_group_name,
+    province_manager_user_number ,
+    province_manager_user_name ,
+    city_manager_user_number ,
+    city_manager_user_name , 
+    sales_manager_user_number ,
+    sales_manager_user_name , 
+    supervisor_user_number ,
+    supervisor_user_name,
+    count(customer_no) all_cn,
+  count(
+    case
+      when typeder = '断约客户' then customer_no
+    end
+  ) cust_cn
+  from (
+select  
+	 a.*,
+	 c.province_manager_user_number ,
+    c.province_manager_user_name ,
+    c.city_manager_user_number ,
+    c.city_manager_user_name , 
+    c.sales_manager_user_number ,
+    c.sales_manager_user_name , 
+    c.supervisor_user_number ,
+    c.supervisor_user_name,
+    b.max_sdt,
+    d.terminate_date,
+    IF(d.customer_code IS not  NULL and b.max_sdt is not null     , '断约客户', NULL) AS typeder
+from tmp_customer_level a 
+left join tmp_sale_detail b on a.customer_no=b.customer_code
+left join 
+(select * from tmp_terminate_info where rn=1) d on a.customer_no=d.customer_code
+ left join 
+          (
+            select *
+            from csx_dim.csx_dim_crm_customer_info
+            where sdt= '20250930'
+         --  and channel_code  in ('1','7','9')
+          ) c on a.customer_no=c.customer_code 
+-- where b.max_sdt is not null 	 
+) a 
+group by  region_name,
+    province_name,
+    city_group_name,
+    province_manager_user_number ,
+    province_manager_user_name ,
+    city_manager_user_number ,
+    city_manager_user_name , 
+    sales_manager_user_number ,
+    sales_manager_user_name , 
+    supervisor_user_number ,
+    supervisor_user_name
+;
+
+
 -- AB类客户按照等级销售经理A类客户 
 -- AB类客户明细
+ 
 with   tmp_customer_level as (select
   region_name,
   province_name,
@@ -843,6 +956,7 @@ left join
 -- where b.max_sdt is not null 	 
  
 ;
+
 
 
 -- A类客户占比-省区总
